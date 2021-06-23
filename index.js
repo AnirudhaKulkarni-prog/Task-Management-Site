5d8c4593ed25f030040dc00d7dcd56214e3a0615
AnirudhaKulkarni-prog <anirudhakulkarni771@gmail.com>
2021-06-23 12:56:24 +0530

    added delete feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const AllData=[];
+var AllData=[];
 const taskContainer=document.querySelector(".task_container");
 console.log(taskContainer);
 
@@ -10,8 +10,12 @@ function generateCard(taskData)
     <div class="col-md-6 col-lg-4" id=${taskData.id}>
               <div class="card">
                 <div class="card-header d-flex justify-content-end gap-2">
-                  <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-                  <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+                  <button type="button" class="btn btn-outline-success">
+                    <i class="fas fa-pencil-alt"></i>
+                  </button>
+                  <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)">
+                    <i class="fas fa-trash" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i>
+                  </button>
                 </div>
                 <img src="${taskData.imageurl}" class="card-img-top" alt="...">
 
@@ -60,6 +64,26 @@ function addData()
         AllData.push(taskData);
         localStorage.setItem("Tasky",JSON.stringify({cards:AllData}));
     
+}
+
+function deleteCard(event)
+{
+  event=window.event;
+  const toDeleteId=event.target.id;
+  const tag=event.target.tagName;
+  AllData = AllData.filter((cardObject)=> cardObject.id !== toDeleteId);
+  localStorage.setItem("Tasky",JSON.stringify({cards:AllData}));
+
+  //contact the parent to delete the child
+
+  if(tag === "BUTTON")
+  {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }
+  else
+  {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
 }
 
 //Page Refresh will erase the added data so use local storage to save the data
